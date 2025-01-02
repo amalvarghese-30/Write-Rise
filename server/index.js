3edd795909448b823db105ac4ddf6b46bad845a5
amalvarghese-30 <156003739+amalvarghese-30@users.noreply.github.com>
2025-01-02 22:04:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,8 +12,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
-
+app.use(cors({
+    origin: 'https://write-rise-frontend.onrender.com', // Allow requests from this domain
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+}));
 
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
