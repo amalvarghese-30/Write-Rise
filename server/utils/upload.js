db63687773aabaee814f6dbfb2a14dc06935063b
amalvarghese-30 <156003739+amalvarghese-30@users.noreply.github.com>
2025-01-02 22:27:28 +0530

    Update upload.js

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -5,20 +5,25 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const storage = new GridFsStorage({
-    url: `mongodb+srv://Abhishek:OS4FmQUVqDOsgGJA@blog-app.b4d8d.mongodb.net/?retryWrites=true&w=majority&appName=blog-app`,
-    options: { useNewUrlParser: true },
-    file: (request, file) => {
-        const match = ["image/png", "image/jpg"];
+    url: process.env.MONGO_URI || `mongodb+srv://Abhishek:OS4FmQUVqDOsgGJA@blog-app.b4d8d.mongodb.net/?retryWrites=true&w=majority&appName=blog-app`,
+    options: { useNewUrlParser: true, useUnifiedTopology: true },
+    file: (req, file) => {
+        const match = ["image/png", "image/jpg", "image/jpeg", "image/webp"];
 
-        if (match.indexOf(file.memeType) === -1)
-            return `${Date.now()}-blog-${file.originalname}`;
+        if (match.indexOf(file.mimetype) === -1) {
+            return {
+                bucketName: "photos",
+                filename: `${Date.now()}-blog-${file.originalname}`,
+            };
+        }
 
         return {
             bucketName: "photos",
-            filename: `${Date.now()}-blog-${file.originalname}`
-        }
+            filename: `${Date.now()}-blog-${file.originalname}`,
+        };
     }
 });
+
 const upload = multer({ storage });
 
 export default upload;
