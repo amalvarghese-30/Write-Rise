bf2a71b89de2dcb85cd3632d2e1911ad782e363e
amalvarghese-30 <156003739+amalvarghese-30@users.noreply.github.com>
2025-01-02 21:31:39 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -4,7 +4,8 @@ const Connection = async () => {
     const URL = `mongodb+srv://Abhishek:OS4FmQUVqDOsgGJA@blog-app.b4d8d.mongodb.net/?retryWrites=true&w=majority&appName=blog-app`;
     try {
         // Updated: No need for useNewUrlParser or useUnifiedTopology
-        await mongoose.connect(URL);
+        await mongoose.connect(URL, { useUnifiedTopology: true });
+
         console.log('Database connected successfully');
     } catch (error) {
         console.log('Error while connecting to the database ', error);
